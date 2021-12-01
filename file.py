05caa2d010437803548435304f86b239129de405
Colby Veal <colbydanielle220@gmail.com>
2021-12-01 02:23:21 -0500

    Updated return values and printing to be aligned with requirements

--- file.py
+++ file.py
@@ -1,10 +1,14 @@
 from library_data_collection import *
+import sys
 
-records = load_data()
-print(json.dumps(records, indent=4))
+if (len(sys.argv) > 1):
+    records = load_data(sys.argv[1])
+else:
+    records = load_data()
+print(*records['return_records'], sep = "\n")
 
 pages = get_total_pages_read(records)
 print('Total Pages Read: ', int(pages))
 
 ddc = get_total_pages_per_ddc(records)
-print (ddc)
+print ('By Catagory: ', ddc)
--- library_data_collection.py
+++ library_data_collection.py
@@ -3,6 +3,32 @@ import math
 from collections import Counter
 from enum import Enum
 
+
+class DDC(Enum):
+    CS_Info_General = 0
+    Phil_Psych = 100
+    Religion = 200
+    Social = 300
+    Language = 400
+    Pure_Science = 500
+    Applied_Science = 600
+    Arts_Rec = 700
+    Lit = 800
+    History_Geo = 900
+
+DDC_Names = {
+    "CS_Info_General": "Computer Science, Information & General Works",
+    "Phil_Psych": "Philosophy & Psychology",
+    "Religion": "Religion",
+    "Social":"Social Sciences",
+    "Language":"Language",
+    "Pure_Science":"Pure Science",
+    "Applied_Science":"Applied Science",
+    "Arts_Rec":"Arts & Recreation",
+    "Lit":"Literature",
+    "History_Geo":"History & Geography"
+}
+
 def load_data(file='test_data.json'):
     with open(file) as f:
         RECORDS = json.load(f)
@@ -24,23 +50,21 @@ def get_total_pages_per_ddc(RECORDS):
     ddc_list = []
     for record in RECORDS['return_records']:
         ddc_list.append(get_ddc_for_value(record['DDC']))
-    #ddc_json = json.dumps(ddc_list)
-    ddc_count = Counter(ddc_list)
-    return ddc_count
+    ddc_names = get_DDC_names(ddc_list)
+    ddc_count = Counter(ddc_names)
+    ddc_json = json.dumps(ddc_count,indent=4)
+    return ddc_json
 
 def get_ddc_for_value(ddc):
     ddc = int(ddc[:-7])
     ddc_floor = math.floor(ddc /100) * 100
     return ddc_floor #returns DDC Category number. Reference DDC(Enum) for values
 
-class DDC(Enum):
-    CS_Info_General = 0
-    Phil_Psych = 100
-    Religion = 200
-    Social = 300
-    Language = 400
-    Pure_Science = 500
-    Applied_Science = 600
-    Arts_Rec = 700
-    Lit = 800
-    History_Geo = 900
+def get_DDC_names(ddc_list):
+    ddc_names = []
+    for ddc_code in ddc_list:
+        ddc_names.append(DDC_Names[(DDC(ddc_code).name)])
+    return ddc_names
+
+
+
